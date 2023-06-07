08cb89024b22738988331af467ab412caa6e5d61
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2023-06-07 15:27:09 -0400

    NavMesh agent setup and basic moving

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    private NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.destination = target.position;
+    }
+}
