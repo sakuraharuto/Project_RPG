e64200367497f51ea35381f04e4773b8ad2c7013
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2023-06-07 22:03:45 -0400

    Click-to-move implemented

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,19 +5,35 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-
-    [SerializeField] private Transform target;
     private NavMeshAgent _navMeshAgent;
+    private Camera _camera;
 
     // Start is called before the first frame update
     void Start()
     {
+        _camera = Camera.main;
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _navMeshAgent.destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            _navMeshAgent.destination = hit.point;
+        }
     }
 }
