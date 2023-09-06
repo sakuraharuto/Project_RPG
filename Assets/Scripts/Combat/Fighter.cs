d5a746e3362b49776b071982fe65f713aee2b24c
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2023-09-06 18:35:11 -0400

    Attack animation trigger

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,10 +10,15 @@ namespace Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] private float weaponRange = 2f;
+        [SerializeField] private float timeBetweenAttack = 1f;
         private Transform _target;
-        
+        private static readonly int Attack1 = Animator.StringToHash("attack");
+        private float timeSinceLastAttack = 0;
+
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+            
             if (_target == null) return;
             
             if (!GetIsInRange())
@@ -23,6 +28,16 @@ namespace Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttack)
+            {
+                GetComponent<Animator>().SetTrigger(Attack1);
+                timeSinceLastAttack = 0;
             }
         }
 
@@ -41,6 +56,12 @@ namespace Combat
         public void Cancel()
         {
             _target = null;
+        }
+        
+        // Animation Event
+        void Hit()
+        {
+            
         }
     }
 }
